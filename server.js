b0ba3ead394538c76fa79f9db95bb32c8035df31
swl979623074 <979623074@qq.com>
2017-07-10 07:42:13 +0800

    upgrade

--- server.js
+++ server.js
@@ -3,26 +3,34 @@ var fs = require("fs");
 var url = require("url");
 
 var server = http.createServer(function(req,res){
-	var path = url.parse(req.url)
+	var path = url.parse(req.url).pathname;
+	var query = url.parse(req.url,true).query;
+	console.log("path: " + path + ",query: " + JSON.stringify(query));
 	
-	if("/" == req.url){
+	if("/" == path){
 		res.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"})
 		res.write("hello");
 		res.end();
 	}
-	if("/index" == req.url){
+	if("/index" == path){
 		fs.readFile("./web/index.html",function(err,data){
 			res.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"})
 			res.write(data);
 			res.end();
 		})
 	}
-	if("/favicon.ico" == req.url){
+	if("/favicon.ico" == path){
 		fs.readFile("./web/icon/favicon.png",function(err,data){
 			res.writeHead(200,{"Content-Type":"text/image"});
 			res.write(data);
 			res.end();
 		})
+	}
+	if("/perison" == path){
+		res.writeHead(200,{"Content-Type":"text/plain;charset=UTF-8"});
+		res.write("返回");
+		res.write(JSON.stringify(query));
+		res.end();
 	}
 })
 server.listen(3000)
